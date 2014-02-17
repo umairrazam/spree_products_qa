62afcab16fc791136dca40e742cc7a5e87d4a28b
Marek Buszman <mbusiq@gmail.com>
2014-02-17 13:10:42 +0100

    some specs and factories

--- lib/spree_products_qa/factories.rb
+++ lib/spree_products_qa/factories.rb
@@ -1,6 +1,13 @@
 FactoryGirl.define do
-  # Define your Spree extensions Factories within this file to enable applications, and other extensions to use and override them.
-  #
-  # Example adding this to your spec_helper will load these Factories for use:
-  # require 'spree_products_qa/factories'
+  factory :answer, class: Spree::Answer do
+    content 'Answer'
+  end
+
+  factory :question, class: Spree::Question do
+    content 'What is the question?'
+    association :product, factory: :base_product
+    factory :question_with_answer do
+      answer
+    end
+  end
 end
--- spec/models/spree/question_spec.rb
+++ spec/models/spree/question_spec.rb
@@ -1,5 +1,37 @@
 require 'spec_helper'
 
 describe Spree::Question do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject { build_stubbed(:question) }
+  context "scopes" do
+    let!(:question_with_answer){ create(:question_with_answer) }
+    let!(:question_without_answer){ create(:question) }
+
+    describe "answered" do
+      it 'returns only answered questions' do
+        expect(described_class.answered).to_not include(question_without_answer)
+      end
+    end
+
+    describe 'not_answered' do
+      it 'returns only questions without answers' do
+        expect(described_class.not_answered).to_not include(question_with_answer)
+      end
+    end
+  end
+
+  context "methods" do
+    describe "answer_for_form" do
+      let(:answer){ build_stubbed(:answer) }
+
+      it 'builds new answer if its not present' do
+        expect(subject).to receive(:build_answer).once
+        subject.answer_for_form
+      end
+
+      it 'returns answer if its present' do
+        subject.stub(:answer).and_return(answer)
+        expect(subject.answer_for_form).to eq answer
+      end
+    end
+  end
 end
