e4c1676b2e29fc85f80a5aaee269fcc66d9b14dc
Marek Buszman <mbusiq@gmail.com>
2014-08-18 15:04:14 +0200

    find_by_permalink no longer works, use slug instead

--- app/controllers/spree/admin/questions_controller.rb
+++ app/controllers/spree/admin/questions_controller.rb
@@ -1,7 +1,7 @@
 module Spree
   module Admin
     class QuestionsController < ResourceController
-      belongs_to 'spree/product', find_by: :permalink
+      belongs_to 'spree/product', find_by: :slug
 
       def index
         @collection = @collection.page params[:page]
