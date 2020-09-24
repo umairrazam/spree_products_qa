700b5213fa9b61fda6e5bd6a231503c4262dcd41
Prakash Poudel <prakash@truemark.com.np>
2020-09-25 02:08:38 +0545

    Fix api

--- app/serializers/spree/v2/storefront/product_question_serializer.rb
+++ app/serializers/spree/v2/storefront/product_question_serializer.rb
@@ -5,7 +5,7 @@ module Spree
         set_type :product_question
 
         attributes :content, :is_visible
-        
+
         has_one :product_answer
         has_one :user
         has_one :product
