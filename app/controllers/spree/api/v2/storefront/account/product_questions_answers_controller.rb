3b2bb5435f52dcc9e345540c4a9a851269fb499d
Prakash Poudel <prakash@truemark.com.np>
2021-01-18 13:15:14 +0545

    Update to resource controller

--- app/controllers/spree/api/v2/storefront/account/product_questions_answers_controller.rb
+++ app/controllers/spree/api/v2/storefront/account/product_questions_answers_controller.rb
@@ -3,7 +3,7 @@ module Spree
     module V2
       module Storefront
         module Account
-          class ProductQuestionsAnswersController < ::Spree::Api::V2::BaseController
+          class ProductQuestionsAnswersController < ::Spree::Api::V2::ResourceController
             before_action :require_spree_current_user
 
             # GET /api/v2/storefront/account/product_questions_answers
