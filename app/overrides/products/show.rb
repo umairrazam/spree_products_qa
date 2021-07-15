f6776b37348b8b8f899c005eebee2a017136f282
omairazam <omairr.azam@gmail.com>
2021-07-16 03:02:41 +0500

    fix(design): fix design of question answer section.

--- app/overrides/products/show.rb
+++
@@ -1,4 +0,0 @@
-Deface::Override.new(virtual_path: 'spree/products/show',
-                     name: 'add_qna_to_product',
-                     insert_bottom: '[data-hook="product_description"]',
-                     partial: 'spree/products/qna')
